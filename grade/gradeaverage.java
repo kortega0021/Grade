74958f497c0ec95305ab6c4a0add26e19e315789
kortega0021 <kayortega1@gmail.com>
2021-05-05 19:56:15 -0400

    first grade

--- grade/gradeaverage.java
+++ grade/gradeaverage.java
@@ -4,6 +4,50 @@ public class gradeaverage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		package Public;
+		import java.util.*;
+		public class Gradeaverage {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		int scores[] = new int[10];
+		int k;
+		float sum =0,ave;
+
+		Scanner aa=new Scanner (System.in)
+		System.out.print("1:");
+		int num1 = Scanner in.nextInt();
+		 
+		System.out.print("2:");
+		int num2 = in.nextInt();
+		 
+		System.out.print("2: ");
+		int num3 = in.nextInt();
+		 
+		System.out.print("3: ");
+		int num4 = in.nextInt();
+
+		System.out.print("4: ");
+		int num5 = in.nextInt();
+
+		System.out.print("6: ");
+		int num7 = in.nextInt();
+
+		System.out.print("7: ");
+		int num8 = in.nextInt();
+
+		System.out.print("8: ");
+		int num9 = in.nextInt();
+
+		System.out.print("9: ");
+		int num10 = in.nextInt();
+
+		System.out.print("10: ");
+		int num10 = in.nextInt();
+
+		System.out.println("Average of five numbers is: " + 
+				   (num1 + num2 + num3 + num4 + num5) / 10);
+
 
 	}
 
